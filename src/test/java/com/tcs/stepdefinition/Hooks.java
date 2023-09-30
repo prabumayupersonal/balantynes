6471625228c11f8ad74eb32a3dc06298a58c2037
prabumayupersonal <143799909+prabumayupersonal@users.noreply.github.com>
2023-09-30 11:41:57 +0530

    Update Hooks.java

--- src/test/java/com/tcs/stepdefinition/Hooks.java
+++ src/test/java/com/tcs/stepdefinition/Hooks.java
@@ -9,16 +9,26 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
+
 public class Hooks extends BaseClass {
 
 	@Before
 	public void setUp() {
 		ExtentService.getInstance().setSystemInfo("Selenium Version", "2.46");
-	
+
+		if (launchBrowser("chrome")) {
+			maximizeBrowser();
+		} else {
+			Assert.assertTrue(false, "Browser launching : ");
+		}
+
 	}
 
 	@After
-	public void tearDown(Scenario scenario ) {
-      
+	public void tearDown(Scenario scenario) {
+		scenario.attach(getBytesScreenshot(), "image/png", scenario.getName());
+		quitBrowser();
+		System.out.println("Quit Browser......");
+
 	}
 }
